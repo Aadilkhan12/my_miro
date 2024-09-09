33e9dc1b5bbb6e4fb84ef5ab4db617013b5ca2b1
aadil <rkaadil06@gmail.com>
2024-09-09 21:17:48 +0530

    create slidbar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+interface DashboardLayoutProps{
+    children:React.ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+    return(
+        <div>
+            {children}
+        </div>
+    )
+};
+
+export default DashboardLayout;
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,8 @@
+const DashboardPage=()=>{
+    return(
+        <div>
+            Root page of Dashboard
+        </div>
+    )
+}
+ export default DashboardPage;
