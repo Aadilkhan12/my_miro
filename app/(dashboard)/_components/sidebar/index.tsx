024289234407c250e27f5e118b06cd087e903877
aadil <rkaadil06@gmail.com>
2024-09-09 21:36:54 +0530

    sidebar layout create

---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,7 @@
+export const Sidebar=()=>{
+    return(
+        <aside className=" fixed h-full left-0 p-3 bg-blue-950 w-[60px] z-[1] flex flex-col gap-y-4">
+            sidebar!
+        </aside>
+    )
+}
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,16 @@
+import { Sidebar } from "./_components/sidebar";
+
 interface DashboardLayoutProps{
     children:React.ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
-    return(
-        <div>
-            {children}
-        </div>
-    )
+    return (
+      <main className="h-full">
+        <Sidebar />
+        <div className="pl-[60px] h-full">{children}</div>
+      </main>
+    );
 };
 
 export default DashboardLayout;
