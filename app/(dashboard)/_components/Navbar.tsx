5ed09b5538f1abf64f6319e880811377aff41612
aadil <rkaadil06@gmail.com>
2024-09-10 20:04:07 +0530

    git navbar layout Create

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,8 +1,16 @@
 "use client"
+
+import { UserButton } from "@clerk/nextjs"
+
  export const Navbar=()=>{
     return(
         <div className=" flex items-center gap-x-4 p-5 bg-green-500">
-            navbar
+
+            <div className=" hidden lg:flex lg:flex-1 bg-yellow-500">
+                search
+            </div>
+            <UserButton/>
+    
 
         </div>
     )
