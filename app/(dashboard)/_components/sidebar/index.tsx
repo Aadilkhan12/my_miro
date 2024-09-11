036e8d2c1f85dea7065f25837d2710635e690d4a
aadil <rkaadil06@gmail.com>
2024-09-11 22:19:30 +0530

    wip  same piage

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,7 +1,9 @@
+import { NewButton } from "./new-button"
+
 export const Sidebar=()=>{
     return(
         <aside className=" fixed h-full left-0 p-3 bg-blue-950 w-[60px] z-[1] flex flex-col gap-y-4 test-white">
-            sidebar!
+            <NewButton/>
         </aside>
     )
 }
---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,25 @@
+"use client ";
+
+import { Plus } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+// import { Dialog, DialogContent, DialogTrigger } from "@components/ui/tooltip";
+import { Dialog, DialogContent, DialogTrigger } from "@radix-ui/react-dialog";
+
+export const NewButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="aspect-square">
+          <button className="bg-white/25 h-full w-full roundesd-md flex item-center justify-center opacity-60 hover:opacity-100 transition">
+            <Plus className=" text-white" />
+          </button>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent  border-none max-w-[480]">
+        <CreateOrganization/>
+
+      </DialogContent>
+
+    </Dialog>
+  );
+};
