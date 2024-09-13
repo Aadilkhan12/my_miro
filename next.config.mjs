007d0c9e58fa63fb2f11897dada2f46192b542b4
aadil <rkaadil06@gmail.com>
2024-09-13 22:20:34 +0530

    fix the bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+  images: {
+    domains: ["img.clerk.com"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
