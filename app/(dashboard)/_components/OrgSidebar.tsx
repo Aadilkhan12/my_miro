a45b061c3b5346630b5d92902efa6270e2e7c398
aadil <rkaadil06@gmail.com>
2024-09-17 21:57:07 +0530

    organisation switcher button

--- app/(dashboard)/_components/OrgSidebar.tsx
+++ app/(dashboard)/_components/OrgSidebar.tsx
@@ -3,24 +3,73 @@ import Link from "next/link";
 import Image from "next/image";
  import { Poppins } from "next/font/google";
  import logo from "../../../public/logo.svg"
-import { cn } from "lib/utils";
+// import { cn } from "../../../lib/utils";
+import { cn } from "../../../lib/utils";
+import { Button } from "../../../src/components/ui/button";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { LayoutDashboard, Star } from "lucide-react";
   const font =Poppins({
     weight:["600"],
     subsets:["latin"]
   })
 export const OrgSidebar = () => {
   return (
-    <div className=" hidden lg:flex fex-col space-x-6 w-[260] pl-5 pt-5">
-      <Link href="/">
-        <div className=" flex items-center gap-x-2">
-          <Image src={logo} alt="Logo" width={60} height={60}/>
-            <span
-              className={cn("font-semibold text-2xl", font.className)}
+  
+      <div className=" hidden lg:flex fex-col space-x-6 w-[260] pl-5 pt-5">
+        <Link href="/">
+          <div className=" flex items-center gap-x-2">
+            <Image src={logo} alt="Logo" width={60} height={60} />
+            <span className={cn("font-semibold text-2xl", font.className)}>
+              Board
+            </span>
+          </div>
+          <OrganizationSwitcher
+            hidePersonal
+            appearance={{
+              elements: {
+                rootBox: {
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                },
+                organizationSwitcherTrigger: {
+                  padding: "6px",
+                  width: "100%",
+                  borderRadius: "8px",
+                  border: "1px solid #E5E7E8",
+                  justifyContent: "space-between",
+                  backgroundColor: "white",
+                },
+              },
+            }}
+          />
+          <div className="space-y-1 w-full">
+            <Button
+              variant={"ghost"}
+              asChild
+              size={"lg"}
+              className="font-normal justify-start px-2 w-full"
             >
-            Board
-        </span>
-        </div>
-      </Link>
-    </div>
+              <Link href="/">
+                <LayoutDashboard className="h-4 w-4 mr-2" />
+                Team Boards
+              </Link>
+            </Button>
+            <Button
+              variant={"ghost"}
+              asChild
+              size={"lg"}
+              className="font-normal justify-start px-2 w-full"
+            >
+              <Link href="/">
+                <Star className="h-4 w-4 mr-2" />
+                Favorite Boards
+              </Link>
+            </Button>
+          </div>
+        </Link>
+      </div>
+
   );
 };
