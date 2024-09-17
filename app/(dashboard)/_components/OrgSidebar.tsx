f454593cf89e14c3825f5aa66274385dffda76c0
aadil <rkaadil06@gmail.com>
2024-09-17 21:16:51 +0530

    resolve  self closing erorr

--- app/(dashboard)/_components/OrgSidebar.tsx
+++ app/(dashboard)/_components/OrgSidebar.tsx
@@ -13,12 +13,12 @@ export const OrgSidebar = () => {
     <div className=" hidden lg:flex fex-col space-x-6 w-[260] pl-5 pt-5">
       <Link href="/">
         <div className=" flex items-center gap-x-2">
-          <Image src={logo} alt="Logo" width={60} height={60}>
+          <Image src={logo} alt="Logo" width={60} height={60}/>
             <span
               className={cn("font-semibold text-2xl", font.className)}
-            ></span>
+            >
             Board
-          </Image>
+        </span>
         </div>
       </Link>
     </div>
