71e8ffafbd235a490b1623a1f642daf2cbb958c3
aadil <rkaadil06@gmail.com>
2024-09-20 21:38:50 +0530

    add invite button  of draw

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,13 +1,40 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, OrganizationSwitcher } from "@clerk/nextjs";
 import {SearchInput} from "./search-input";
+import { InviteButton } from "./invite-button";
 export const Navbar = () => {
   return (
     <div className=" flex items-center gap-x-4 p-5">
       <div className=" hidden lg:flex lg:flex-1">
-        <SearchInput/>
-        </div>
+        <SearchInput />
+      </div>
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                marginBottom: "5px",
+                alignItems: "center",
+                width: "100%",
+                maxWidth:"376px"
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #E5E7E8",
+                justifyContent: "space-between",
+                backgroundColor: "white",
+              },
+            },
+          }}
+        />
+      </div>
+      <InviteButton/>
       <UserButton />
     </div>
   );
