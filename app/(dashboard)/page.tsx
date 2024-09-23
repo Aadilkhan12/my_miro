9185bb001fd159d9267bf7ac1d16988f77e9a09e
aadil <rkaadil06@gmail.com>
2024-09-23 22:44:02 +0530

    empty state

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,4 @@
 const DashboardPage = () => {
-  return <div>Root page of Dashboard</div>;
+  return <div className="bg-red-500 flex-1 h-[calc(100%-80px)] p-6">Root page</div>;
 };
 export default DashboardPage;
