3e6224ebb63e6a866891c8b31918eac5cde0373a
aadil <rkaadil06@gmail.com>
2024-09-27 22:12:59 +0530

    empty state

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+interface BoardListProps{
+    orgId:string;
+    query:{
+        search?:string;
+        favorites?:string;
+    }
+};
+
+ export const BoardList=({orgId,query}:BoardListProps)=>{
+    const data =[];//TODO: change for API call
+    if(!data?.length &&query.search)
+    {
+        return(
+            <div>
+                Try searching  for something else
+            </div>
+        )
+    }
+    if(!data?.length && query.favorites)
+    {
+        return(
+            <div>
+                No favorites
+            </div>
+        );
+    }
+    if(!data.length )
+    {
+        return <div>No board at all</div>;
+    
+    }
+    return(
+        <div>
+            {JSON.stringify(query)}
+        </div>
+    )
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
+import {BoardList} from "./_components/board-list";
 
 interface DashboardPageProps{
   searchParams :{
@@ -12,14 +13,16 @@ interface DashboardPageProps{
 const DashboardPage = ({searchParams}:DashboardPageProps) => {
   const {organization} =useOrganization();
   return <div className=" flex-1 h-[calc(100%-80px)] p-6">
-    {JSON.stringify(searchParams)}
     {
       !organization ?(  <EmptyOrg/>):
-      (<p>
-        Board List!
-      </p>)
+      (<BoardList
+        orgId={organization.id}
+        query={searchParams}
+        />
+      )
+    
     }
-  
+
     </div>;
 };
 export default DashboardPage;
