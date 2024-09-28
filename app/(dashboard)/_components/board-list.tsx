0f894056749f1f55e96c186a00786dc388cda6dd
aadil <rkaadil06@gmail.com>
2024-09-28 18:17:00 +0530

    create empty search page

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,39 +1,25 @@
-"use client"
+"use client";
 
-interface BoardListProps{
-    orgId:string;
-    query:{
-        search?:string;
-        favorites?:string;
-    }
-};
+import { EmptySearch } from "./empty-search";
 
- export const BoardList=({orgId,query}:BoardListProps)=>{
-    const data =[];//TODO: change for API call
-    if(!data?.length &&query.search)
-    {
-        return(
-            <div>
-                Try searching  for something else
-            </div>
-        )
-    }
-    if(!data?.length && query.favorites)
-    {
-        return(
-            <div>
-                No favorites
-            </div>
-        );
-    }
-    if(!data.length )
-    {
-        return <div>No board at all</div>;
-    
-    }
-    return(
-        <div>
-            {JSON.stringify(query)}
-        </div>
-    )
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
 }
+
+export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = []; //TODO: change for API call
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
+  if (!data?.length && query.favorites) {
+    return <div>No favorites</div>;
+  }
+  if (!data.length) {
+    return <div>No board at all</div>;
+  }
+  return <div>{JSON.stringify(query)}</div>;
+};
