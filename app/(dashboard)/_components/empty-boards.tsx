20679fe5ac9260902084ddec9307b978e7216c72
aadil <rkaadil06@gmail.com>
2024-10-03 23:32:55 +0530

    setup create  board shema

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,22 @@
+"use client";
+import { useMutation } from "convex/react";
+import { api } from "convex/_generated/api";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useOrganization } from "@clerk/nextjs";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const create = useMutation(api.board.create);
+  const onClick =()=>{
+    if(!organization)
+      return;
+    create({
+      orgId:organization.id,
+      title:"Untitled"
+
+    })
+  }
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src={"/note.png"} height={110} width={110} alt="Empty" />
@@ -10,7 +25,7 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6"></div>
-      <Button size="lg">Create board</Button>
+      <Button onClick={onClick} size="lg">Create board</Button>
     </div>
   );
 };
